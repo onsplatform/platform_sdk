2a6e7bf8a5a35eb63bec43a25151e3c151ad220a
Bruno Leitão <bleitao@ons.org.br>
2019-04-26 15:02:32 -0300

    'sdk'

--- schema/schema_api.py
+++ schema/schema_api.py
@@ -1,18 +1,25 @@
 import requests
 
-from schema.command import *
+from pony import orm
 
 
 class SchemaApi:
     url_schema_api = "http://localhost/schema/{0}/{1}/{2}"
 
+    def __init__(self, db):
+        self.db = db
+
     def get_schema(self, solution, app, _map):
         api_response = self._get_schema_response(solution, app, _map)
         api_response = api_response.json()
-        model = api_response['model']
-        fields = self._get_fields(api_response['fields'])
-        filters = self._get_filter(api_response['filter'])
-        return SelectCommand(model, fields, filters)
+
+        model = self._get_model(api_response['model'], api_response['fields'])
+
+        proxy_model = model.build(self.db)
+        self.db.generate_mapping(create_tables=True)
+
+        with orm.db_session():
+            return list(orm.select(d for d in proxy_model))
 
     def _get_schema_response(self, solution, app, _map):
         response = requests.get(self._get_schema_api_url(solution, app, _map))
@@ -26,4 +33,32 @@ class SchemaApi:
         return Filter(_filter['name'], _filter['expression'])
 
     def _get_fields(self, fields):
-        return [Field(f['name'], f['alias']) for f in fields]
+        return [RemoteField(f['alias'], str, f['name']) for f in fields]
+
+    def _get_model(self, model, fields):
+        return RemoteMap(model['name'], model['table'], self._get_fields(fields))
+
+
+class RemoteField:
+    def __init__(self, name, field_type, column, required=True):
+        self.name = name
+        self.field_type = field_type
+        self.column = column
+        self.required = required
+
+    def build(self):
+        wrapper = orm.Required if self.required else orm.Optional
+        return wrapper(self.field_type, column=self.column)
+
+
+class RemoteMap:
+    def __init__(self, name, table, fields):
+        self.name = name
+        self.table = table
+        self.fields = fields
+
+    def build(self, db):
+        fields = {f.name: f.build() for f in self.fields}
+        fields['_table_'] = self.table
+        dyn_type = type(self.name, (db.Entity, ), fields)
+        return dyn_type
