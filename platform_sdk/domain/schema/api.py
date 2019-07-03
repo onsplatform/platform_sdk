014a94bf964d669d3bdacad3e329db28ac082abe
Bruno Leitão <bleitao@ons.org.br>
2019-07-03 14:51:47 -0300

    include type

--- platform_sdk/domain/schema/api.py
+++ platform_sdk/domain/schema/api.py
@@ -8,11 +8,11 @@ class SchemaApi:
         self.base_uri = schema_settings['uri']
         self.client = HttpClient()
 
-    def get_schema(self, _map):
-        uri = self.get_uri(_map)
+    def get_schema(self, _map, _type):
+        uri = self.get_uri(_map, _type)
         result = self.client.get(uri)
         if not result.has_error:
             return result.content
 
     def get_uri(self, _map):
-        return  os.path.join(self.base_uri, _map)
+        return  os.path.join(self.base_uri, _map, _type)
