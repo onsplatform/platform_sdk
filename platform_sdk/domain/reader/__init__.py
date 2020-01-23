4324eda3d737ef1a152af07b567042ce6ee9c38b
Lucas Thimoteo <lucas.thimoteo@radixeng.com.br>
2020-01-23 12:42:25 -0300

    Domain reader get_entities method implementation

--- platform_sdk/domain/reader/__init__.py
+++ platform_sdk/domain/reader/__init__.py
@@ -2,16 +2,14 @@
 from platform_sdk.utils.http import HttpClient
 
 
-class ReaderApi:
-    def __init__(self, reader_settings):
-        self.base_uri = reader_settings['uri']
+class DomainReaderApi:
+    def __init__(self, uri):
+        self.base_uri = uri
         self.client = HttpClient()
 
-    def get_entities(self, _map, _type, _params):
-        uri = self.get_uri(_map, _type, _params)
-        result = self.client.get(uri)
-        if not result.has_error and result.content:
-            return result.content[0]
+    def get_map_entities(self, _map, _type, _params):
+        uri = self._mount_uri(_map, _type, _params)
+        return self.client.get(uri)
 
-    def get_uri(self, _map, _type, _params):
+    def _mount_uri(self, _map, _type, _params):
         return '{}{}/{}?{}'.format(self.base_uri, _map, _type, _params)
